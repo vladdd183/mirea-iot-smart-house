5b484c22ed1f70d31217340111e230bb08f336fd
vladdd183 <vladdd183@gmail.com>
2022-04-21 16:26:20 +0300

    update

--- IOT/base.py
+++ IOT/base.py
@@ -4,26 +4,26 @@ from random import randint
 from time import sleep
 
 class device():
-    def __init__(self, client_id = None):
-        self.broker = config['broker']
-        self.port = config['port']
-        self.home_topic = config['home_topic']
+    def __init__(self, home_topic, room, name, broker='broker.hivemq.com', port=1883, username='test', password='test'):
+        self.home_topic = home_topic
+        self.room = room
+        self.name = name
+        self.topic = f'{home_topic}/{room}/{name}'
 
-        if not client_id:
-            self.client_id = f'python-mqtt-{randint(0, 1000)}'
-        else:
-            self.client_id = client_id
+        self.client_id = f'python-mqtt-{randint(0, 1000)}'
 
 
         self.client = mqtt.Client(self.client_id)
+        self.client.username_pw_set(username, password)
         
         self.client.on_message = self.on_message
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
         
-        self.client.connect(self.broker, self.port)
+        self.client.connect(broker, port)
         
         self.client.subscribe(self.home_topic)
+        self.client.subscribe(self.topic)
         self.client.loop_start()
 
         self.run()
@@ -31,18 +31,35 @@ class device():
     def on_message(self, client, userdata, msg):
         topic = msg.topic
         payload = str(msg.payload.decode())
-        print(topic.center(70,'='), '\n', payload, '\n', '-'*70)
+        
+        if 'annonce' == payload:
+            data = {'name': self.name,
+                    'room': self.room,
+                    'topic': self.topic,
+                    'return': 'annonce'}
+            self.client.publish(self.home_topic, str(data)) 
+        
+        self.message_handler(payload)    
+
 
     def on_connect(self, client, obj, flags, rc):
-        print(obj, flags, rc)
+        data = {'name': self.name,
+                'room': self.room,
+                'topic': self.topic,
+                'return': 'connected'}
+        self.client.publish(self.home_topic, str(data)) 
         
     def on_disconnect(self, client, obj, rc):
-        print(obj, flags, rc)
+        data = {'name': self.name,
+                'room': self.room,
+                'topic': self.topic,
+                'return': 'disconnected'}
+        self.client.publish(self.home_topic, str(data)) 
 
     def run(self):
         while True:
-            self.client.publish(self.home_topic, f'{randint(0,100)}')
-            sleep(1)
-
-a = device()
+            pass
+    
+    
+a = device('home1', 'room4', 'dev1')
 
--- IOT/config.py
+++ IOT/config.py
@@ -2,5 +2,5 @@ config = {}
 
 config['broker'] = 'broker.hivemq.com'
 config['port'] = 1883
-config['home_topic'] = 'mirea/iot/home'
+config['home_topic'] = 'home1'
 
